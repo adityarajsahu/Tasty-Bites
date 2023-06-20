a8e10442dd67193097d725a99abb458752a823f9
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2023-06-20 14:37:25 +0530

    cart button component added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 
+import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/cover.jpg";
 import classes from "./Header.module.css";
 
@@ -7,8 +8,8 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Tasty Bites</h1>
-        <button>Cart</button>
+        <h1 className={classes.title}>Tasty Bites</h1>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
