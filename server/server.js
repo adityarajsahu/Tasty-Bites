9eae4737025cee4e77b054f468c5009378277170
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2024-03-30 00:02:03 +0530

    orders post route

--- server/server.js
+++ server/server.js
@@ -19,6 +19,40 @@ app.get("/meals", async (req, res) => {
     res.json(JSON.parse(data));
 });
 
+app.post("/orders", async (req, res) => {
+    const orderData = req.body.order;
+
+    if (orderData === null || orderData.items === null || orderData.items.length === 0) {
+        return res.status(400).json({ message: "data missing" });
+    }
+
+    if (
+        orderData.customer.email === null ||
+        !orderData.customer.email.includes("@") ||
+        orderData.customer.name == null ||
+        orderData.customer.name.trim() === "" ||
+        orderData.customer.street === null ||
+        orderData.customer.street.trim() === "" ||
+        orderData.customer["postal-code"] === null ||
+        orderData.customer["postal-code"].trim() === "" ||
+        orderData.customer.city === null ||
+        orderData.customer.city.trim() === ""
+    ) {
+        return res.status(400).json({ message: "missing data: email, name, street, postal code or city is missing." });
+    }
+
+    const newOrder = {
+        ...orderData,
+        id: (Math.random() * 1000).toString(),
+    };
+
+    const orders = await fs.readFile("../server/data/orders.json", "utf8");
+    const allOrders = JSON.parse(orders);
+    allOrders.push(newOrder);
+    await fs.writeFile("../server/data/orders.json", JSON.stringify(allOrders));
+    res.status(201).json({ message: "order created" });
+});
+
 app.use((req, res) => {
     if (req.method === "OPTIONS") {
         res.sendStatus(200);
