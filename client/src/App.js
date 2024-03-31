16f4057fe95b5071e839698574362e1e99f40b12
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2024-04-01 02:50:41 +0530

    remove item logic and updated cart button done

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import Header from "./components/Header";
 import Meals from "./components/Meals";
 
+import { CartContextProvider } from "./store/CartContext";
+
 function App() {
     return (
-        <React.Fragment>
+        <CartContextProvider>
             <Header />
             <Meals />
-        </React.Fragment>
+        </CartContextProvider>
     );
 }
 
